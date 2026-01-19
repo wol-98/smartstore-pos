92b2b2106e914f0fdf84249a8776760d3866c5a1
wol-98 <raphaelwol20@gmail.com>
2026-01-20 01:41:36 +0530

    feat: Add ViewController to serve homepage

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+
     @GetMapping("/")
     public String home() {
+        // This looks for "index.html" inside src/main/resources/templates/
         return "index"; 
     }
 }
